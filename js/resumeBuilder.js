1edb7ebdf2c9c2c217b5bab8ccafa7e930660a66
frandman <franjlopez@MacBook-Pro-de-Esther.local>
2015-04-10 08:38:11 +0200

    Name and Role formatting

---
+++ js/resumeBuilder.js
@@ -0,0 +1,53 @@
+
+/* Variable formatting */
+
+var formattedHeaderName = HTMLheaderName.replace("%data%", "Fran Lopez");
+var formattedHeaderRole = HTMLheaderRole.replace("%data%","Front-End Developer");
+
+var formattedcontactGeneric = HTMLcontactGeneric;
+var formattedmobile = HTMLmobile;
+var formattedemail = HTMLemail;
+var formattedtwitter = HTMLtwitter;
+var formattedgithub = HTMLgithub;
+var formattedblog = HTMLblog;
+var formattedlocation = HTMLlocation;
+
+var formattedbioPic = HTMLbioPic;
+var formattedwelcomeMsg = HTMLwelcomeMsg;
+
+var formattedskillsStart = HTMLskillsStart;
+var formattedskills = HTMLskills;
+
+var formattedworkStart = HTMLworkStart;
+var formattedworkEmployer = HTMLworkEmployer;
+var formattedworkTitle = HTMLworkTitle;
+var formattedworkDates = HTMLworkDates;
+var formattedworkLocation = HTMLworkLocation;
+var formattedworkDescription = HTMLworkDescription;
+
+var formattedprojectStart = HTMLprojectStart;
+var formattedprojectTitle = HTMLprojectTitle;
+var formattedprojectDates = HTMLprojectDates;
+var formattedprojectDescription = HTMLprojectDescription;
+var formattedprojectImage = HTMLprojectImage;
+
+var formattedschoolStart = HTMLschoolStart;
+var formattedschoolName = HTMLschoolName ;
+var formattedschoolDegree = HTMLschoolDegree;
+var formattedschoolDates = HTMLschoolDates;
+var formattedschoolLocation = HTMLschoolLocation;
+var formattedschoolMajor = HTMLschoolMajor;
+
+var formattedonlineClasses = HTMLonlineClasses;
+var formattedonlineTitle = HTMLonlineTitle;
+var formattedonlineSchool = HTMLonlineSchool;
+var formattedonlineDates = HTMLonlineDates;
+var formattedonlineURL = HTMLonlineURL;
+
+
+/* Filling the content */
+
+$("#header").append(formattedHeaderName);
+$("#header").append(formattedHeaderRole);
+
+
