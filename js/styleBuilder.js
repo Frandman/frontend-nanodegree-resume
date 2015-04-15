41683737ff1ef99273bd5566a0a9ce7c65b796fc
frandman <franjlopez@MacBook-Pro-de-Esther.local>
2015-04-15 08:04:50 +0200

    bootstrap grid

---
+++ js/styleBuilder.js
@@ -0,0 +1,21 @@
+
+$('body').prepend('<script src"js/bootstrap.min.js"></script>');
+$('#main').addClass('container');
+$('#header span:first').replaceWith( '<h4 id="position">'+bio.role+'</h4>' );
+$('#name, #position').wrapAll('<div class="row nameRole"><div class="col-xs-12 col-sm-6 col-sm-offset-6 text-right"></div></div>');
+$('ul').addClass('list-inline');
+$('hr').remove();
+$('#topContacts').addClass('row');
+$('#topContacts li').addClass('col-xs-12 col-sm-4');
+$('img').addClass('img-responsive');
+$('.biopic, .welcome-message, #skillsH3, #skills').wrapAll('<div class="row"></div>');
+$('.biopic').addClass('col-xs-12 col-sm-6');
+$('#projects, .project-entry, #workExperience  h2, .work-entry').addClass('row');
+$('.project-entry img').wrap('<div class="col-xs-12 col-sm-6"></div>');
+$('#education, .education-entry, #education  h2, #education h3, .education-entry').addClass('row');
+$('#mapDiv, #mapDiv h2, map').addClass('row');
+$('#letsConnect').addClass('row');
+
+
+
+
