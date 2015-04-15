de5d22a73476587091c40373316f2a0d0040f46c
frandman <franjlopez@MacBook-Pro-de-Esther.local>
2015-04-15 21:26:37 +0200

    background fonts borders

--- js/styleBuilder.js
+++ js/styleBuilder.js
@@ -1,5 +1,6 @@
 
 $('body').prepend('<script src"js/bootstrap.min.js"></script>');
+$('body').prepend('<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" >');
 $('#main').addClass('container');
 $('#header span:first').replaceWith( '<h2 id="position">'+bio.role+'</h4>' );
 $('#name, #position').wrapAll('<div class="row nameRole"><div class="col-xs-12 col-sm-6 col-sm-offset-6 text-right"></div></div>');
@@ -9,12 +10,15 @@ $('#topContacts').addClass('row');
 $('#topContacts li').addClass('col-xs-12 col-sm-4');
 $('img').addClass('img-responsive');
 $('.biopic, .welcome-message, #skillsH3, #skills').wrapAll('<div class="row"></div>');
-$('.biopic').addClass('col-xs-12 col-sm-6');
+$('.welcome-message, #skillsH3, #skills').wrapAll('<div class="col-xs-12 col-sm-6"></div>')
+$('.biopic').wrap('<div class="col-xs-12 col-sm-6"></div>');
 $('#projects, .project-entry, #workExperience  h2, .work-entry').addClass('row');
 $('.project-entry img').wrap('<div class="col-xs-12 col-sm-6"></div>');
 $('#education, .education-entry, #education  h2, #education h3, .education-entry').addClass('row');
 $('#mapDiv, #mapDiv h2, map').addClass('row');
 $('#letsConnect').addClass('row');
+ $('h2.row, h3.row').attr('id', 'no-border');
+
 
 
 
